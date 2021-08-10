f15af00c226b97e8e3f18f10cdcc9111b931f3c0
Azhar U <88520158+azhar35@users.noreply.github.com>
2021-08-10 16:49:38 -0400

    added project files
    added project files

---
+++ src/main/java/com/skillstorm/employee/TelecomCustomerPortalApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomCustomerPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomCustomerPortalApplication.class, args);
+	}
+
+}
