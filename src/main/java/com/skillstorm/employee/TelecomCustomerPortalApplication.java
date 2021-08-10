6e27a2723190815ab6398f3b601a62eef5813d1a
Vscrl0 <adbeal@ucdavis.edu>
2021-08-10 14:13:40 -0700

    This is a test

--- src/main/java/com/skillstorm/employee/TelecomCustomerPortalApplication.java
+++ src/main/java/com/skillstorm/employee/TelecomCustomerPortalApplication.java
@@ -3,6 +3,7 @@ package com.skillstorm.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test comment
 @SpringBootApplication
 public class TelecomCustomerPortalApplication {
 
